57d86b10473bf196816e51775d336c7854aced25
Carlos Roa S <CarlosRoa>
2025-11-20 14:16:37 -0300

    update hackaton 2

--- mobile-backup/src/navigation/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Exporta los navegadores
- */
-
-export {AppNavigator} from './AppNavigator';
-export {ClinicalNavigator} from './ClinicalNavigator';
-export {PatientNavigator} from './PatientNavigator';
-export * from './types';
